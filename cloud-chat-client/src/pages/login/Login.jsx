c2c88997cba7773749a05dc90a2469b8778d60cc
eescal01 <106106910+eescal01@users.noreply.github.com>
2025-07-03 11:00:43 -0500

    [feat] add login

--- cloud-chat-client/src/pages/login/Login.jsx
+++ cloud-chat-client/src/pages/login/Login.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import './Login.css';
 import assets from "../../assets/assets";
-
+// <img src={assets.logo_big} alt="logo" className="logo" />
 const Login = () => {
     return (
         <div className="login">
-            <img src={assets.logo_big} alt="logo" className="logo" />
-
+            
+            here we are
         </div>
     )
 }
