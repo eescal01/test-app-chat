b3e11e3b686702025d422178f8b3cee2bab889f0
eescal01 <106106910+eescal01@users.noreply.github.com>
2025-07-03 12:17:48 -0500

    [feat] add login

---
+++ cloud-chat-client/src/components/Hero.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import assets from "../assets/assets";
+import "./Hero.css";
+
+const CommunityHero = () => (
+  <div className="hero-container">
+    <div className="hero-background" style={{ backgroundImage: `url(${assets.background})` }}>
+      <div className="hero-content">
+        <h1>Connect with your community</h1>
+        <p>
+          Connect is where you can make a home for your communities and friends. Where you can stay close and have fun over text, voice, and video.
+        </p>
+        <button className="hero-btn">Try the App</button>
+      </div>
+    </div>
+    <footer className="hero-footer">
+      <div className="hero-socials">
+        <a href="#"><i className="fa-brands fa-twitter"></i></a>
+        <a href="#"><i className="fa-brands fa-instagram"></i></a>
+        <a href="#"><i className="fa-brands fa-facebook"></i></a>
+      </div>
+      <div className="hero-copyright">
+        ©2025 Connect
+      </div>
+    </footer>
+  </div>
+);
+
+export default CommunityHero;
--- cloud-chat-client/src/main.jsx
+++ cloud-chat-client/src/main.jsx
@@ -6,6 +6,7 @@ import App from './App';
 import Callback from './Callback';
 import ChatRoom from './ChatRoom';
 import Login from './pages/login/Login';
+import CommunityHero from './components/Hero';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -15,6 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/callback" element={<Callback />} />
         <Route path="/chat" element={<ChatRoom />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/Hero" element={<CommunityHero />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
