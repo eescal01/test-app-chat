b5c766d4c3e4ac6fa4f6621fcf5939d35c4e7754
eescal01 <106106910+eescal01@users.noreply.github.com>
2025-07-03 16:36:11 -0500

    [feat] add login

--- cloud-chat-client/src/components/Footer.jsx
+++ cloud-chat-client/src/components/Footer.jsx
@@ -3,37 +3,45 @@ import "./Footer.css";
 
 const Footer = () => (
   <footer className="footer">
-    <div className="footer-content">
-      <div className="footer-links">
-        <div className="footer-section">
+    <div className="footer-main">
+      <div className="footer-description">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
+      
+      <div className="footer-links-group">
+        <div className="footer-column">
           <a href="#">About Us</a>
           <a href="#">Jobs</a>
           <a href="#">Press</a>
           <a href="#">Blog</a>
         </div>
-        <div className="footer-section">
+        
+        <div className="footer-column">
           <a href="#">Contact Us</a>
           <a href="#">Terms</a>
           <a href="#">Privacy</a>
         </div>
-        <div className="footer-section">
-          <div className="footer-contact">
-            <p>+1-543-123-4567</p>
-            <p>example@connect.com</p>
-          </div>
-        </div>
       </div>
+      
+      <div className="footer-contact">
+        <p>+1-543-123-4567</p>
+        <p>example@connect.com</p>
+      </div>
+      
       <div className="footer-socials">
         <a href="#" aria-label="Facebook">
           <i className="fa-brands fa-facebook"></i>
         </a>
-        <a href="#" aria-label="Twitter">
+        <a href="#" aria-label="twitter">
           <i className="fa-brands fa-twitter"></i>
         </a>
         <a href="#" aria-label="Instagram">
           <i className="fa-brands fa-instagram"></i>
         </a>
       </div>
+      <div className="footer-copyright">©2025 Connect</div>
     </div>
   </footer>
 );
