8f56acf40440776166a26b6f21884625a18e7d01
eescal01 <106106910+eescal01@users.noreply.github.com>
2025-07-03 22:54:19 -0500

    [feat] add login

--- cloud-chat-client/src/main.jsx
+++ cloud-chat-client/src/main.jsx
@@ -2,17 +2,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App';
-import Callback from './Callback';
-import ChatRoom from './ChatRoom';
+import { Home, Callback, Chat } from './pages';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Home />} />
         <Route path="/callback" element={<Callback />} />
-        <Route path="/chat" element={<ChatRoom />} />
+        <Route path="/chat" element={<Chat />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
