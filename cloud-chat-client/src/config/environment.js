63e58bb798e6f7b3d5bd8c9a373c577068ee5895
eescal01 <106106910+eescal01@users.noreply.github.com>
2025-07-06 15:38:56 -0500

    [feat] add login

--- cloud-chat-client/src/config/environment.js
+++ cloud-chat-client/src/config/environment.js
@@ -1,6 +1,6 @@
 // src/config/environment.js
 export const COGNITO_DOMAIN = 'https://google-auth-domain-dev.auth.us-east-1.amazoncognito.com';
-export const CLIENT_ID = '7uj34k419euao5ndikbipaqqn0';
+export const CLIENT_ID = '17thot8aa53q64es4d9ppuduuh';
 export const REDIRECT_URI = 'https://main.d2p1llmpk1i4a9.amplifyapp.com/callback';
 export const RESPONSE_TYPE = 'code';
 export const SCOPE = 'openid+email+profile'; 
